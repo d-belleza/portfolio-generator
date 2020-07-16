4490e169288654b7cf934c6a5ee68cd625148d46
davidbelleza <davidbelleza@gmail.com>
2020-07-16 17:17:10 -0500

    generate new HTML

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -125,22 +128,15 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
 });
 
-
-
-
-
-
-
-
-
-
-
-
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
 // const profileDataArgs = process.argv.slice(2, process.argv.length);
 // const [name, github] = profileDataArgs;
 
